74f7c08eed995d569e2482a299e6a9deb4758229
sadwikandula2 <123432491+sadwikandula2@users.noreply.github.com>
2024-05-16 15:54:22 -0700

    Greedy-1 Done

--- sample.java
+++ sample.java
@@ -1,7 +1,82 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Problem1 Jump Game (https://leetcode.com/problems/jump-game/)
 
 
-// Your code here along with comments explaining your approach
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : yes
+
+class Solution {
+    public boolean canJump(int[] nums) {
+        int p=nums.length-1;
+        for(int i=nums.length-2;i>=0;i--)
+        {
+            if(nums[i]+i>=p)
+                p=i;
+        }
+        return p==0;
+    }
+}
+
+
+           
+//Problem2 Jump Game II (https://leetcode.com/problems/jump-game-ii/)
+
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : yes
+
+
+class Solution {
+    public int jump(int[] nums) {
+        int jumps=0;
+        int farthest=0;
+        int curr=0;
+        for(int i=0;i<nums.length-1;i++)
+        {
+            farthest=Math.max(farthest,nums[i]+i);
+            if(i==curr)
+            {
+                jumps++;
+                curr=farthest;
+                if(curr>=nums.length-1)
+                {
+                    break;
+                }
+            }
+        }
+        return jumps;
+    }
+}
+
+
+// Problem3 Candy (https://leetcode.com/problems/candy/)
+
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : yes
+
+class Solution {
+    public int candy(int[] ratings) {
+        int []res=new int[ratings.length];
+        Arrays.fill(res,1);
+        for(int i=1;i<ratings.length;i++)
+        {
+            if(ratings[i]>ratings[i-1])
+            {
+                res[i]=res[i-1]+1;
+            }  
+        }
+        int sum=res[res.length-1];
+        for(int i=ratings.length-2;i>=0;i--)
+        {
+            if(ratings[i]>ratings[i+1])
+            {
+                res[i]=Math.max(res[i+1]+1,res[i]);
+            }
+            sum+=res[i]; 
+        }
+        return sum;
+    }
+}
+
+
